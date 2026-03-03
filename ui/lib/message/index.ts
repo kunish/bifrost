d8f2d83ddd5c7ab108e7c88583ea82df2c36c734
Suresh Chaudhary <hello@suresh.im>
2026-03-03 13:48:15 +0530

    feat/prompt-playground-ui-components

---
+++ ui/lib/message/index.ts
@@ -0,0 +1,17 @@
+export { Message } from './message'
+export {
+  MessageRole,
+  MessageType,
+  type APIMessage,
+  type CompletionRequest,
+  type CompletionResult,
+  type CompletionResultChoice,
+  type MessageContent,
+  type MessageError,
+  type MessageImageURL,
+  type MessageMediaURL,
+  type SerializedMessage,
+  type ToolCall,
+  type ToolCallFunction,
+  type ToolResult,
+} from './types'
